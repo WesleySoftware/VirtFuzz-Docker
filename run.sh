0f1c7de5e9f9824fee8247afdfd5f97f67fa2687
Wesley <95096973+WesleySoftware@users.noreply.github.com>
2025-02-09 15:55:00 -0500

    Add back privileged flag to docker
    Before, I could run the fuzzer in unprivileged docker, I guess thats not
    true anymore. Adding back the privileged flag removes the QEMU bus
    errors
    
    TODO figure out the correct capabilities to give the container so it
    doesnt just run in god mode

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ set -eu
 
 
 #be sure to run it passing-through kvm
-docker run --device=/dev/kvm -it $(docker build -q .) /bin/bash
+docker run --privileged --device=/dev/kvm -it $(docker build -q .) /bin/bash
 
